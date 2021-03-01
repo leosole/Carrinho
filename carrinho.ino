e843956f4221728e99a80b702d59723d75b10519
leosole <leosole@gmail.com>
2021-03-01 08:56:58 -0300

    Add files via upload

--- carrinho.ino
+++ carrinho.ino
@@ -1,149 +1,163 @@
-#define IN1 6                       //Pinos motor A (Direita)
-#define IN2 7                       //Pinos motor A (Direita)
-#define IN3 8                       //Pinos motor B (Esquerda)
-#define IN4 9                       //Pinos motor B (Esquerda)
-#define ENA 10                      //Pino velocidade motor A (Enable A)
-#define ENB 11                      //Pino velocidade motor B (Enable B)
-#define V0 25						//Velocidade baixa
-#define V1 50						//Velocidade média
-#define V2 100						//Velocidade alta
+#define IN1 7                       //Pinos motor A (Direita)
+#define IN2 8                       //Pinos motor A (Direita)
+#define IN3 9                       //Pinos motor B (Esquerda)
+#define IN4 10                       //Pinos motor B (Esquerda)
+#define ENA 5                      //Pino velocidade motor A (Enable A)
+#define ENB 6                      //Pino velocidade motor B (Enable B)
+#define V0 90            //Velocidade baixa
+#define V1 100           //Velocidade média
+#define V2 110            //Velocidade alta
 
+char data[5] = "0123";
+unsigned long t0 = 0;
 
 void setup() {
-	pinMode(IN1,OUTPUT);
-	pinMode(IN2,OUTPUT);
-	pinMode(IN3,OUTPUT);
-	pinMode(IN4,OUTPUT);
-	pinMode(ENA,OUTPUT);
-	pinMode(ENB,OUTPUT);
-	// Transferi essas chamadas para dentro do loop
-	// analogWrite(ENA,110);
-	// analogWrite(ENB,110);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  Serial.begin(9600);
+  t0 = millis();
 }
 
-char cha;
-
 void loop() {
-	if (Serial.available() > 0) {
-		cha = Serial.read();
-		delay(2);
-		//Movimenta para frente
-		if(cha == 'C') { // 22
-			analogWrite(ENA,V2);
-			analogWrite(ENB,V2);
-			digitalWrite(IN1,HIGH);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,HIGH);
-			digitalWrite(IN4,LOW);
-		}
-		else if(cha == 'V') { // 11
-			analogWrite(ENA,V1);
-			analogWrite(ENB,V1);
-			digitalWrite(IN1,HIGH);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,HIGH);
-			digitalWrite(IN4,LOW);
-		}
-		//Movimenta para trás
-		else if(cha == 'B') {  // -2-2
-			analogWrite(ENA,V2);
-			analogWrite(ENB,V2);
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,HIGH);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,HIGH);
-		}
-		else if(cha == 'N') {  // -1-1
-			analogWrite(ENA,V1);
-			analogWrite(ENB,V1);
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,HIGH);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,HIGH);
-		}
-		//movimenta para direita
-		else if(cha == 'R') {  // 20
-			analogWrite(ENB,V1);
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,HIGH);
-			digitalWrite(IN4,LOW);
-		}
-		else if(cha == 'T') {  // 10
-			analogWrite(ENB,V0);
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,HIGH);
-			digitalWrite(IN4,LOW);
-		}
-		else if(cha == 'U') {  // -10
-			analogWrite(ENB,V0);
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,HIGH);
-		}
-		else if(cha == 'Y') {  // 21
-			analogWrite(ENA,V0);
-			analogWrite(ENB,V1);
-			digitalWrite(IN1,HIGH);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,HIGH);
-			digitalWrite(IN4,LOW);
-		}
-		else if(cha == 'I') {  // -2-1
-			analogWrite(ENA,V0);
-			analogWrite(ENB,V1);
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,HIGH);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,HIGH);
-		}
-		//movimenta para esquerda
-		else if(cha == 'F') {  // 02
-			analogWrite(ENA,V1);
-			digitalWrite(IN1,HIGH);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,LOW);
-		}
-		else if(cha == 'G') {  // 01
-			analogWrite(ENA,V0);
-			digitalWrite(IN1,HIGH);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,LOW);
-		}
-		else if(cha == 'J') {  // 0-1
-			analogWrite(ENA,V0);
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,HIGH);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,LOW);
-		}
-		else if(cha == 'H') {  // 12
-			analogWrite(ENA,V1);
-			analogWrite(ENB,V0);
-			digitalWrite(IN1,HIGH);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,HIGH);
-			digitalWrite(IN4,LOW);
-		}
-		else if(cha == 'K') {  // -2-1
-			analogWrite(ENA,V1);
-			analogWrite(ENB,V0);
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,HIGH);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,HIGH);
-		}
-		//Sem movimento
-		// else if(cha == '0') {
-		else {
-			digitalWrite(IN1,LOW);
-			digitalWrite(IN2,LOW);
-			digitalWrite(IN3,LOW);
-			digitalWrite(IN4,LOW);
-		}
-	}
+  if (Serial.available() > 3) {
+    data[0] = Serial.read();
+    data[1] = Serial.read();
+    data[2] = Serial.read();
+    data[3] = Serial.read();
+
+    String command = String(data); // Para poder comparar
+    delay(2);
+
+    //Movimenta para frente
+    if (command.equals("+2+2")) { // 22
+      analogWrite(ENA, V2);
+      analogWrite(ENB, V2);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+    }
+    else if (command.equals("+1+1")) { // 11
+      analogWrite(ENA, V1);
+      analogWrite(ENB, V1);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+    }
+    //Movimenta para trás
+    else if (command.equals("-2-2")) { // -2-2
+      analogWrite(ENA, V2);
+      analogWrite(ENB, V2);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+    }
+    else if (command.equals("-1-1")) { // -1-1
+      analogWrite(ENA, V1);
+      analogWrite(ENB, V1);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+    }
+    //movimenta para direita
+    else if (command.equals("+2+0")) { // 20
+      analogWrite(ENB, V1);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+    }
+    else if (command.equals("+1+0")) { // 10
+      analogWrite(ENB, V0);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+    }
+    else if (command.equals("-1+0")) { // -10
+      analogWrite(ENB, V0);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+    }
+    else if (command.equals("+2+1")) { // 21
+      analogWrite(ENA, V0);
+      analogWrite(ENB, V1);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+    }
+    else if (command.equals("-2-1")) { // -2-1
+      analogWrite(ENA, V0);
+      analogWrite(ENB, V1);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+    }
+    //movimenta para esquerda
+    else if (command.equals("+0+2")) { // 02
+      analogWrite(ENA, V1);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+    }
+    else if (command.equals("+0+1")) { // 01
+      analogWrite(ENA, V0);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+    }
+    else if (command.equals("+0-1")) { // 0-1
+      analogWrite(ENA, V0);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+    }
+    else if (command.equals("+1+2")) { // 12
+      analogWrite(ENA, V1);
+      analogWrite(ENB, V0);
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+    }
+    else if (command.equals("-1-2")) { // -1-2
+      analogWrite(ENA, V1);
+      analogWrite(ENB, V0);
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+    }
+    //Sem movimento
+    // else if(command.equals("+0+0")) {
+    else {
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+    }
+  }
+  else if (millis() - t0 > 2000) { // 2 segundos
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+
+    t0 = millis();
+  }
 }
